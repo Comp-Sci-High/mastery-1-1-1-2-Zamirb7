be43ff308126701476574996c3df70a1a550a344
Zamirb7 <Zamir.Ballee26@compscihigh.org>
2025-11-16 23:19:52 +0000

    First 3 questions answered

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 // Don't overthink the answers, anything is fine!
 // SECTION 1
 console.log("Q1. If you had to give a TED talk on anything, what would it be?")
-console.log("...")
+console.log("One Piece")
 
 console.log("Q2. What's the last show/movie you watched that you would recommend?")
-console.log("...")
+console.log("Stranger Things")
 
 console.log("Q3. What is an activity you do to relax or destress?")
-console.log("...")
+console.log("Duolingo")
 // Commit here! Reference README.md for section 2
 
 // SECTION 3
 console.log("Q4. What's something you're really proud of but don't get to talk about often?")
-console.log("...")
+console.log(". . .")
 
 console.log("Q5. If you could travel anywhere, where would you go?")
-console.log("...")
+console.log(". . .")
 // Commit here!
